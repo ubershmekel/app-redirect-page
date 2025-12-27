6a6f1fe8e8134c47481cba7a2d5c1877b7305003
Yuval Greenfield <ubershmekel@gmail.com>
2025-12-26 23:08:39 -0800

    Git push when publishing

--- scripts/release.js
+++ scripts/release.js
@@ -1,4 +1,5 @@
 const { execSync } = require("child_process");
 
 execSync("npm version patch", { stdio: "inherit" });
+execSync("git push --follow-tags", { stdio: "inherit" });
 execSync("npm publish", { stdio: "inherit" });
