d19613fd492ce8e4689b4e212038ef2481540163
Yuval Greenfield <ubershmekel@gmail.com>
2025-12-29 10:22:28 -0800

    No need for a "prepublish" step, it's in the release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,6 @@
 const { execSync } = require("child_process");
 
+execSync("npm run build", { stdio: "inherit" });
 execSync("npm version patch", { stdio: "inherit" });
 execSync("git push --follow-tags", { stdio: "inherit" });
 execSync("npm publish", { stdio: "inherit" });
